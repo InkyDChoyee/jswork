5dcbf6ea531bb9aa35930f3478a65fcedc38e937
InkyDChoyee <choyee0421@naver.com>
2023-11-14 17:03:37 +0900

    coffee works

--- day5/JS/coffee.js
+++ day5/JS/coffee.js
@@ -7,3 +7,21 @@ for(let i = 0; i < samllPics.length; i++){
         bigPic.setAttribute("src", this.src);
     };
 }
+
+// 상세 설명 보이기/숨기기(토글 구현)
+let sw = true;
+let detail = document.querySelector("#detail");
+let view = document.querySelector("#view"); // 상세설명 보기
+
+// 객체의 onclick 속성을 사용하여 구현
+view.onclick = function() {  // 이름없는 실행 함수
+    if(sw) {
+        detail.style.display = "block";
+        view.innerText = "상세 설명 닫기"
+        sw = false; // 상태 변경
+    }else {
+        detail.style.display = "none";
+        view.innerText = "상세 설명 보기"
+        sw = true; // 상태 변경
+    }
+}
